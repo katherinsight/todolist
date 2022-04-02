f6ceb67e3a2dad067eb7751014b295401a730750
katherinsight <nkmn98@naver.com>
2022-04-02 08:28:04 +0000

    디자인 적용

---
+++ src/components/Todolist.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    안뇽안뇽
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from 'HelloWorld.vue'
+import Todolist from 'Todolist.vue'
+
+	
+export default {
+  name: 'todolist',
+  components: {
+	  HelloWorld,
+	  Todolist
+  },
+}
+</script>
